7abb8375b71bf7ccd80853964e0ebbbb832186c7
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-11-05 12:17:16 -0300

    número informado pelo usuario na tela

--- Aula 01 - Tipos de Dados e Operadores Aritméticos/ex02.py
+++ Aula 01 - Tipos de Dados e Operadores Aritméticos/ex02.py
@@ -5,3 +5,6 @@
 #	 * Faça um Programa que peça um número e então mostre a mensagem:
 #    * O número informado foi [número].
 #
+
+numero_usuario = int(input('Insira um número qualquer: '))
+print(f'O número informado foi: {numero_usuario}')
