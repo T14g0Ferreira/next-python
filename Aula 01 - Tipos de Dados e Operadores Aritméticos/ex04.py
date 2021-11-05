a17473a5b9fca2f071877e8e6f199ef5d0862428
T14g0Ferreira <meutrabalho.github@gmail.com>
2021-11-05 12:05:37 -0300

    média final de 4 notas bimestrais

--- Aula 01 - Tipos de Dados e Operadores Aritméticos/ex04.py
+++ Aula 01 - Tipos de Dados e Operadores Aritméticos/ex04.py
@@ -4,3 +4,12 @@
 #
 #	 * Faça um Programa que peça as 4 notas bimestrais e mostre a média.
 #
+
+
+nota1 = float(input('Nota do 1º bimestre: '))
+nota2 = float(input('Nota do 2º bimestre: '))
+nota3 = float(input('Nota do 3º bimestre: '))
+nota4 = float(input('Nota do 4º bimestre: '))
+
+notafinal = (nota1+nota2+nota3+nota4)/4
+print(f'A sua média foi: {notafinal}')
